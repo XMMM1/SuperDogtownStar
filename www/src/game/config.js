66d5c8ab33e43725eca97d9842add69e03f7097f
Matej Murn <matej.murnmatej.murn@gmail.com>
2014-12-13 00:24:08 +0100

    nit commit

---
+++ www/src/game/config.js
@@ -0,0 +1,22 @@
+pandaConfig = {
+    name: 'Dogtown',
+    version: '1.0.1',
+    
+    system: {
+        width: 768,
+        height: 1024,
+        rotateScreen: false,
+        bgColor: '#000000'
+    },
+    analytics: {
+        id: 'UA-57407089-1'
+    },
+    storage: {
+        id: 'net.pandajs.flyingdog'
+    },
+    iOS8: {
+        system: {
+            webGL: true
+        }
+    }
+};
